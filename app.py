8ed0171fbed0ee9efcceb4b3673162f7f18e64e6
Dylan Nakandakari Sugimoto <dylan-ns@hotmail.com>
2019-11-06 15:01:22 -0300

    change debug to false

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
 	db.create_all()
 
 	port = int(os.environ.get("PORT", 5000))
-	app.run(host='0.0.0.0', port=port, debug=True)
+	app.run(host='0.0.0.0', port=port)
 	#app.run(ssl_context='adhoc')
 
     
